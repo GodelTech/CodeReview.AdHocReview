f7205cbf12c0fbfb027eb86c1d4dbe7c77c40bcc
pNovikau <p.novikau@godeltech.com>
2022-02-11 17:33:33 +0300

    [windows] reworked file system and module architecture

---
+++ src/cloc/README.md
@@ -0,0 +1,21 @@
+# CodeReview.AdHocReview [Cloc]
+
+## Requirements
+
+- Powershell - https://github.com/PowerShell/powershell/releases
+- Docker Engine - https://docs.docker.com/engine/install/
+
+## Command Arguments
+
+| Arguments             | Required | Type   | Description argument               |
+|-----------------------|----------|--------|------------------------------------|
+| SolutionDirectoryPath | True     | String | Path to the solution folder        |
+| Output                | True     | String | Path to the output directory       |
+
+## Example
+
+- Clone [CodeReview.AdHocReview](https://github.com/GodelTech/CodeReview.AdHocReview)
+- Clone [CodeReview.Orchestrator](https://github.com/GodelTech/CodeReview.Orchestrator) (can be any project you want to analyze)
+- Navigate to `CodeReview.AdHockCheck\src\cloc\` folder
+- Run `.\run-analysis.ps1 -SolutionDirectoryPath C:\CodeReview.Orchestrator -Output C:\output`
+- All artifacts will be copy to the output folder 
---
+++ src/scripts/run-workflow.psm1
@@ -0,0 +1,47 @@
+function Start-Orchestrator {
+param (
+    [Parameter(Mandatory)][string] $workflowFilePath,
+    [Parameter(Mandatory)][string] $outputDirectoryPath,
+    [Parameter(Mandatory)][string] $importDirectoryPath
+)
+    Write-Host "Creating docker container godeltech/codereview.orchestrator..."
+    
+    docker create --name orchestrator -v /var/run/docker.sock:/var/run/docker.sock godeltech/codereview.orchestrator run -f workflow.yaml
+    if (-not $?) {
+        Write-Error -Message "Failed to create a container for the $workflow workflow"
+    
+        return $False
+    }
+    
+    Write-Host "Copy workflow file to the container..."
+    docker cp $workflowFilePath orchestrator:/app/workflow.yaml
+    if (-not $?) {
+        Write-Error -Message "Cannot copy workflow file to the container"
+    
+        docker rm orchestrator
+    
+        return $False
+    }
+    
+    Write-Host "Copy import directory to the container..."
+    docker cp $importDirectoryPath orchestrator:/app/imports
+    if (-not $?) {
+        Write-Error -Message "Cannot copy import directory to the container"
+    
+        docker rm orchestrator
+    
+        return $False
+    }
+
+    Write-Host "Running container..."
+    docker start -a orchestrator
+
+    Write-Host "Exporting artifacts..."
+    docker cp orchestrator:/app/artifacts $outputDirectoryPath
+
+    Write-Host "Removing container..."
+    docker rm orchestrator
+
+    return $True
+}
+
